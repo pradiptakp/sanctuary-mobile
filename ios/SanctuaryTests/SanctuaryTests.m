663dc63c49dcf2eaf452937f7492206b4d800db0
Angga Pradipta <anggaa.pradipta@gmail.com>
2020-05-14 00:44:47 +0700

    rename to Sanctuary

--- ios/SanctuaryTests/SanctuaryTests.m
+++ ios/SanctuaryTests/SanctuaryTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNStarterTests : XCTestCase
+@interface SanctuaryTests : XCTestCase
 
 @end
 
-@implementation RNStarterTests
+@implementation SanctuaryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
